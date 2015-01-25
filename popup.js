b3828d93a2a05cba85b12b30c9e73709a6b296ca
Mentor Gashi <mentorgashi@vuzzu.net>
2015-01-25 02:25:14 +0100

    Replacing library.js with api.js also including get vitals function on popup.js

--- popup.js
+++ popup.js
@@ -1,6 +1,24 @@
+function updateVitals(username,apiKey) {
+
+   var vitals = performProtectedRequest("edge","vitals",username,apiKey);
+
+   vitals.success(function (data) {
+      $("header .username a").append( data['vitals'].username );
+      $("header .balance a").append( data['vitals'].balance.toString().replace(/,/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",") );
+   });
+
+}
+
 document.addEventListener("DOMContentLoaded", function () {
+
     backGround = chrome.extension.getBackgroundPage();
     backGround.updateIcon();
+
+    var optionsButton = document.getElementById('options');
+    optionsButton.onclick = function() {
+      chrome.tabs.create({'url': chrome.extension.getURL('options.html'), 'selected': true});
+   }
+
 });
 
 jQuery( document ).ready( function( $ ) {
